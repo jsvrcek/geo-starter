183f5284afc5dd70fac5e96542151d37c9ce09e7
jsvrcek <joesvrcek@hotmail.com>
2024-03-09 13:18:21 -0500

    adds admin user

---
+++ api/migrations/0003_auto_20240309_1812.py
@@ -0,0 +1,16 @@
+from django.db import migrations
+from django.contrib.auth.models import User
+
+
+def create_admin_user(apps, schema_editor):
+    User.objects.create_superuser('admin', 'admin@example.com', 'admin')
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ('api', '0002_feature'),
+    ]
+
+    operations = [
+        migrations.RunPython(create_admin_user),
+    ]
